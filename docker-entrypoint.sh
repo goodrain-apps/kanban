4f8f558804971ef5f0e299e21834188caa2d9e5b
zhouyq <zyq916@gmail.com>
2016-07-20 11:47:51 +0800

    add redis test script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,13 @@
 
 KANBAN_REDIS_ADDR=${REDIS_HOST:-127.0.0.1}:${REDIS_PORT:-6379}
 
+# test redis connection
+for i in {30..0}; do
+  if nc -w 1  -v $REDIS_HOST $REDIS_PORT; then
+  break
+  fi
+  echo 'Waiting redis start...'
+  sleep 1
+done
+
 /opt/kanban/kanban server
